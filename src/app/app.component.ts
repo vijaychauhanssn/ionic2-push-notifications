4660a62c02907038e85977c0ce6ce7bb964e9a77
vijaychauhanssn <vijaychauhanssn@gmail.com>
2017-08-18 16:56:51 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,19 +4,34 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import {
+  Push,
+  PushToken
+} from '@ionic/cloud-angular';
+
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, public push: Push, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+    this.push.register().then((t: PushToken) => {
+  return this.push.saveToken(t);
+}).then((t: PushToken) => {
+  console.log('Token saved:', t.token);
+});
+this.push.rx.notification()
+  .subscribe((msg) => {
+    alert(msg.title + ': ' + msg.text);
+  });
   }
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,29 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { CloudSettings, CloudModule } from '@ionic/cloud-angular';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+const cloudSettings: CloudSettings = {
+  'core': {
+    'app_id': '12f768ca'
+  },
+
+  'push': {
+    'sender_id': '638350245748',
+    'pluginConfig': {
+      'ios': {
+        'badge': true,
+        'sound': true
+      },
+      'android': {
+        'iconColor': '#343434'
+      }
+    }
+  }
+};
 
 @NgModule({
   declarations: [
